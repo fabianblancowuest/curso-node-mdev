530436c005c8d59900447d879ce1c19e0d62d2b8
Fabian Blanco Wuest <fabianblancowuest@gmail.com>
2024-05-28 22:05:28 -0300

    update

---
+++ core-modules/process.js
@@ -0,0 +1,15 @@
+// Proporciona información y control sobre el proceso actual de ejecución
+
+// argumento de entrada
+console.log(process.argv);
+
+// controlar el proceso de salida
+// process.on("exit", () => {
+// 	// limpiar los recursos
+// });
+
+// current working directory
+console.log(process.cwd());
+
+// platform
+console.log(process.cpuUsage());
